1751ee895ed7cc1948c500df2620d9dc21ea0d5d
Marcinkiewicz0710 <35112188+Marcinkiewicz0710@users.noreply.github.com>
2019-09-07 18:34:16 +0200

    Update HashTable.py

--- HashTable.py
+++ HashTable.py
@@ -1,3 +1,7 @@
+"""
+Hash Table using collections package
+"""
+import collections
 count = collections.Counter()
 
 # To add/delete an element into the counter
@@ -13,3 +17,26 @@ for item, freq in count.most_common():
 	print(item + ' apprears ' + freq +' time(s).')
 
 # If an element does not exist in the collection then count[x] will return 0
+
+
+"""
+Hash Table using Dictionary
+"""
+# Initialization and basic operations
+dict = {}
+dict['Lucie'] = 1
+length = len(dict)
+value = dict.get('Lucie')
+
+# Return the value of the item and remove the item from the dictionary, return False if the item does not exist
+value = dict.pop('Lucie', False)
+
+# To iterate through the dictionary
+for key, value in dict.iteritems():
+	print(' The value of ' + key + ' is ' + value)
+
+# To check if an item is in the dictionary
+if item in dict:
+	print('Bingo')
+
+
