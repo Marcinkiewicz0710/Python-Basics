36d552bccdcbc53ba8bfccf4bc14a68e191cc31b
Marcinkiewicz0710 <35112188+Marcinkiewicz0710@users.noreply.github.com>
2019-09-08 19:08:07 +0200

    Create List.py

---
+++ List.py
@@ -0,0 +1,20 @@
+# Iteration
+# Print 5,4,3,2,1,0
+for i in range(5,-1,-1):
+	print(i)
+# Print 3,4,5
+for j in range(3,6):
+	print(j)
+
+# Define matrix
+table = [[0]*n for i in range(m)]
+
+# Remove all duplicate elements
+l = list(set(l))
+
+# Insert an element at a specific index
+# e.g. [1,2,3].insert(2,4) will give [1,2,4,3]
+l.insert(<index>, <element>)
+
+# Count the number of an element in the list
+<list>.count(<element>)
