9fcbcdaa6bc2cc4fe615e0d52825e70900c990fb
Marcinkiewicz0710 <35112188+Marcinkiewicz0710@users.noreply.github.com>
2019-09-10 19:06:26 +0200

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -1,1 +1,4 @@
-
+import pandas as pd
+# Read file csv/excel
+df_1 = pd.read_csv(<file.csv>, sep=';', dtype=<dictionary of columns>, decimal='.')
+df_2 = pd.read_excel(<file.xls>, sheetname=<int or list of int>, dtype=<dictionary of columns>)
