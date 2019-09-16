1fdce4cacaac2e7bc9d6fb3457f54c0faa738680
Marcinkiewicz0710 <35112188+Marcinkiewicz0710@users.noreply.github.com>
2019-09-16 17:06:15 +0200

    Update Machine_Learning.py

--- Machine_Learning.py
+++ Machine_Learning.py
@@ -13,13 +13,54 @@ for k in range(1,31):
 	scores = cross_val_score(knn, x_train, x_test, cv=10, scoring='accuracy')
 	k_scores.append(scores.mean())
 	
-	
+
+
+#################################################################
+#                       Basic Methods                           #
+#################################################################
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
+
+# Linear Regression
+lin_reg = LinearRegression()
+rMSE = cross_val_score(lin_reg, x_train, x_test, cv=10, scoring='neg_mean_squared_error').mean()
+
+# Ridge Regression
+rid_reg = Ridge()
+param = {'alpha': [0.001, 0.01, 0.1, 1, 2, 5, 10, 20]}
+search = GridSearchCV(estimator=rid_reg, param, scoring='neg_mean_squared_error', cv=10)
+search.fit(x_train, x_test)
+# To see the parameters
+print(search.best_params_)
+print(search.best_score_)
+
+# Lasso Regression
+lasso_reg = Lasso()
+param = {'alpha': [0.001, 0.01, 0.1, 1, 2, 5, 10, 20]}
+search = GridSearchCV(estimator=lasso_reg, param, scoring='neg_mean_squared_error', cv=10)
+search.fit(x_train, x_test)
+# To see the parameters
+print(search.best_params_)
+print(search.best_score_)
+
+# Random Forest CLASSIFIER (for continuous data, use RandomForestRegressor)
+forest = RandomForestClassifier(n_estimators=100,   # The number of trees in the forest
+                               bootstrap=True,      # Whether bootstrap samples are used when building trees
+                               max_features='sqrt'  # The number of features to consider when looking for the best split
+)
+forest.fit(x_train, x_test)
+# Actual class predictions
+rf_predictions = forest.predict(test)
+# Probabilities for each class
+rf_probs = forest.predict_proba(test)[:, 1]
+roc_value = roc_auc_score(test_labels, rf_probs)
 	
 
 #################################################################
 #                    Elastic Net Regression                     #
 #################################################################
-from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import ElasticNet
 elastic = ElasticNet(normalize=False)
 # Use Cross Validation to Determine the best parameters
@@ -63,6 +104,8 @@ rmse = np.sqrt(mean_squared_error(y_test, preds))
 print("RMSE: %f" % (rmse))
 
 # XGBoost with K-fold Cross Validation
+## XGBoost specific data structure
+data_dmatrix = xgb.DMatrix(data=X, label=y)
 params = {"objective":"reg:linear",'colsample_bytree': 0.3,'learning_rate': 0.1,
                 'max_depth': 5, 'alpha': 10}
 cv_results = xgb.cv(dtrain=data_dmatrix,       
@@ -75,3 +118,8 @@ cv_results = xgb.cv(dtrain=data_dmatrix,
 		    seed=123	               # for reproducibility of results
 )
 
+# Feature Importance
+xg_reg = xgb.train(params=params, dtrain=data_dmatrix, num_boost_round=10)
+xgb.plot_importance(xg_reg)
+plt.rcParams['figure.figsize'] = [20, 20]
+plt.show()
