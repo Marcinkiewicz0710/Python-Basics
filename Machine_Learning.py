52a9f34e98c317edf9707a513e1dd5ccf1a7a7fd
Marcinkiewicz0710 <35112188+Marcinkiewicz0710@users.noreply.github.com>
2019-09-16 11:44:19 +0200

    Update Machine_Learning.py

--- Machine_Learning.py
+++ Machine_Learning.py
@@ -1,1 +1,14 @@
+from sklearn.model_selection import train_test_split, cross_val_score
+#################################################################
+#                      Cross Validation                         #
+#################################################################
+# Train/Test Split
+x_train, x_test, y_train, y_test = train_test_split(Train.drop('SalePrice'), Train.SalePrice, random_state=4)
 
+# Cross Validation
+k_scores = []
+for k in range(1,31):
+	knn = KNeighborsClassifier(n_neighbors = k)
+	scores = cross_val_score(knn, x_train, x_test, cv=10, scoring='accuracy')
+	k_scores.append(scores.mean())
+	
