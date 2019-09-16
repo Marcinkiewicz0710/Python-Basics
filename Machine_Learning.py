f08104d0f6fe295327c22b992354405240a0a77e
Marcinkiewicz0710 <35112188+Marcinkiewicz0710@users.noreply.github.com>
2019-09-16 11:44:48 +0200

    Update Machine_Learning.py

--- Machine_Learning.py
+++ Machine_Learning.py
@@ -1,4 +1,5 @@
 from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
 #################################################################
 #                      Cross Validation                         #
 #################################################################
